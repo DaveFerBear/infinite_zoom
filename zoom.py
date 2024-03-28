aa9c1334c16ff4e633d2f62f6bddf00471124c32
David Ferris <david@phonic.ai>
2024-03-27 23:13:52 -0400

    clean

--- zoom.py
+++ zoom.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 import argparse
 import json
+import base64
 
 load_dotenv()
 UPSCALE_URL = os.getenv('UPSCALE_URL')
@@ -11,22 +12,22 @@ WORK_FOLDER = 'tmp'
 
 def upscale_4x(init_b64: str):
     body = {
-        'init_image': init_b64,
+        'init_image': f'data:image/png;base64,{init_b64}',
         'height': 1024,
         'width': 1024,
         'scale': 2,
 
         # TODO: Can we do something interesting w/ prompt?
         # Typically just provide the original, but we could add a captioning step, ex. llava
-        'prompt': ''
+        'prompt': 'A dog'
     }
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {PAI_API_KEY}'
     }
-    response = requests.post('', json=body, headers=headers)
+    response = requests.post(UPSCALE_URL, headers=headers, data=json.dumps(body))
+
     if (response.status_code != 200):
-        print('Error:', response.text)
         raise Exception(response.text)
     output_b64 = json.loads(response.text)
     return output_b64
@@ -46,28 +47,45 @@ def save_last_frame(vid_path: str, output_img_path):
     os.system(f'ffmpeg -sseof -3 -i {vid_path} -update 1 -q:v 1 {output_img_path}')
 
 def infinite_zoom(base_img_path: str, steps: int):
-    base_img = base_img_path
     video_segments = []
+
+    # copy the base image to the WORK_FOLDER
+    img_0 = f'{WORK_FOLDER}/base-frame-0.png'
+
+    # Copy base image to img_0, resizing to 1024x1024
+    os.system(f'convert {base_img_path} -resize 1024x1024 {img_0}')
+
+    # Print size and dimms of initial image
+    os.system(f'identify {base_img_path}')
+        
     for i in range(steps):
-        # TODO: resize image to 1024x1024 (or w/e the init size is)
+        print(f'Upscaling image {i}...')
 
-        segment = f'{WORK_FOLDER}/segment-{i}.png'
-        convert_img_to_zoomed_video(base_img, segment)
-        video_segments.append(segment)
+        # upscale the image
+        upscaled_img_path = f'{WORK_FOLDER}/upscaled-frame-{i}.png'
+        with open(base_img_path, 'rb') as image_file:
+            base_b64 = base64.b64encode(image_file.read()).decode('utf-8')
+        upscaled_b64 = upscale_4x(base_b64)
+        with open(upscaled_img_path, 'wb') as f:
+            f.write(upscaled_b64)
 
-        last_frame_path = f'{WORK_FOLDER}/base-frame-{i}.png'
-        save_last_frame(segment, last_frame_path)
 
-        # base_b64 = open(last_frame_path, 'rb').read()
-        # upscaled_b64 = upscale_4x(base_b64)
-
-        upscaled_path = f'{WORK_FOLDER}/upscaled-frame-{i}.png'
-        # with open(upscaled_path, 'wb') as f:
-        #     f.write(upscaled_b64)
-
-        mock_upscale_4x(last_frame_path, upscaled_path)
-
-        base_img = upscaled_path
+#         segment = f'{WORK_FOLDER}/segment-{i}.png'
+#         convert_img_to_zoomed_video(base_img, segment)
+#         video_segments.append(segment)
+# 
+#         last_frame_path = f'{WORK_FOLDER}/base-frame-{i}.png'
+#         save_last_frame(segment, last_frame_path)
+# 
+#         # base_b64 = open(last_frame_path, 'rb').read()
+#         # upscaled_b64 = upscale_4x(base_b64)
+# 
+#         # with open(upscaled_path, 'wb') as f:
+#         #     f.write(upscaled_b64)
+# 
+#         mock_upscale_4x(last_frame_path, upscaled_path)
+# 
+#         base_img = upscaled_path
 
     print('Done upscaling images.')
     print('Creating video...')
@@ -77,7 +95,7 @@ def infinite_zoom(base_img_path: str, steps: int):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Infinitely zoom in on an image.')
     parser.add_argument('image', help='A path to the init image.')
-    parser.add_argument('-s', '--steps', type=int, default=10, help='The number of steps to zoom in.')
+    parser.add_argument('-s', '--steps', type=int, default=3, help='The number of steps to zoom in.')
     args = parser.parse_args()
 
     print(f'Running infinite zoom on {args.image} with {args.steps} steps.')
